8533bfff1de2ac7d60c7cb10dad303e5839bd8b9
Charles Reverand <20057606+taliyos@users.noreply.github.com>
2022-06-03 17:27:35 -0400

    Created and Setup Project
    Includes Phaser, Typescript, ESLint, and Parcel (for builds)

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import Phaser from "phaser";
+
+// Scenes
+import StartMenu from "./scenes/StartMenu";
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    physics: {
+    },
+    scene: [StartMenu]
+}
+
+export default new Phaser.Game(config);
