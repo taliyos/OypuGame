f4f32621100f78e4a9ebe52c5b748fb14f8897a9
Charles Reverand <20057606+taliyos@users.noreply.github.com>
2022-06-03 19:25:44 -0400

    Started experimentation with start menu

--- src/scenes/StartMenu.ts
+++ src/scenes/StartMenu.ts
@@ -5,14 +5,23 @@ export default class StartMenu extends Phaser.Scene {
         super("StartMenu");
     }
 
-    create() {
-        const particles = this.add.particles("red");
+    // Load in all necessary assets
+    // (buttons, backgrounds, etc.)
+    preload() {
+        const loader = this.load.image("play_btn", "./src/assets/menu/play.png");
+    }
 
-        const emitter = particles.createEmitter({
+    create() {
+        const particleManager: Phaser.GameObjects.Particles.ParticleEmitterManager = this.add.particles("play_btn");
+
+        const emitter: Phaser.GameObjects.Particles.ParticleEmitter = particleManager.createEmitter({
             speed: 100,
             scale: { start: 1, end: 0},
             blendMode: "ADD"
         });
+        
+        const playBtn: Phaser.GameObjects.Image = this.add.image(this.scale.width / 2, this.scale.height / 2, "play_btn");
+        playBtn.setInteractive();
 
         emitter.start();
     }
